af22871bfc291ec59e2ba82459ab9f3d58352fa0
ponnipa <33538593+ponnipa-PJ@users.noreply.github.com>
2021-04-18 11:15:23 +0700

    update lextoapi

--- app.py
+++ app.py
@@ -1,33 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from pythainlp import word_tokenize
+from pythainlp.util import rank
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-
-data = [
-        {
-            "id": 1,
-            "library": "Pandas",
-            "language": "Python"
-        },
-        {
-            "id": 2,
-            "library": "requests",
-            "language": "Python"
-        },
-        {
-            "id": 3,
-            "library": "NumPy",
-            "language": "Python"
-        }
-    ]
+cors = CORS(app, resources={r"/lexto": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def hello():
-    return "Hello Flask-Heroku"
+    return "Welcome"
 
 
-@app.route('/api', methods=['GET'])
+@app.route('/lexto', methods=['GET'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def get_api():
-    return jsonify(data)
+    text = request.args.get('text')
+    proc = word_tokenize(text)
+    print(proc)
+    return jsonify(proc)
 
 if __name__ == "__main__":
     app.run(debug=False)
